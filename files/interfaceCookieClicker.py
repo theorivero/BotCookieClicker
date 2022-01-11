736d2f74d3d76a102ffd505c272d71f2d275e88d
theorivero <thrivero1@gmail.com>
2022-01-11 08:07:05 -0300

    Treats excepts to stop breaking the bot WIP

--- files/interfaceCookieClicker.py
+++ files/interfaceCookieClicker.py
@@ -21,20 +21,16 @@ class InterfaceCookieClicker(Bot):
         threading.Thread(target=self.buy_helpers).start()
         threading.Thread(target=self.save_game).start()
 
-    def click_on_cookie(self, seconds=10, sleep_time=5):
+    def click_on_cookie(self, seconds=30, sleep_time=5):
         while True:
-            if self.at_main_game:
-                self.clicking = True
+            try:
                 cookie = self.find_element_by_xpath('//*[@id="bigCookie"]')
                 start_click = time.time()
                 while (time.time() - start_click) < seconds:
                     with self.lock:
                         cookie.click()
-                self.clicking = False
-                # time.sleep(sleep_time)
-            else:
-                print('Não foi possivel clicar agora, tentando mais tarde')
-                # time.sleep(sleep_time)
+            except Exception as e:
+                print(f'erro {e} no click on cookie')
 
     def make_upgrades(self):
         while True:
@@ -54,10 +50,9 @@ class InterfaceCookieClicker(Bot):
                     try:
                         enabled_helper.click()
                     except Exception as e:
-                        print(f'erro {e} no make upgrades')
+                        print(f'erro {e} no buy helpers')
 
     def load_game(self):
-        self.at_main_game = False
         time.sleep(1)
         self.click_on_element('//*[@id="prefsButton"]')
         self.click_on_element('//*[@id="menu"]/div[3]/div[3]/a[2]')
@@ -68,20 +63,22 @@ class InterfaceCookieClicker(Bot):
         time.sleep(1)
         self.click_on_element('//*[@id="menu"]/div[1]')
         time.sleep(1)
-        self.at_main_game = True
 
     def save_game(self):
         while True:
-            with self.lock:
+            try:
                 if (time.time() - self.last_save) / 60 > self.minutes_to_save:
-                    time.sleep(1)
-                    self.click_on_element('//*[@id="prefsButton"]')
-                    self.click_on_element('//*[@id="menu"]/div[3]/div[3]/a[1]')
-                    new_save_code = self.get_text_from_element('//*[@id="textareaPrompt"]')
-                    open('save', 'w').write(new_save_code)
-                    self.click_on_element('//*[@id="promptOption0"]')
-                    self.click_on_element('//*[@id="menu"]/div[1]')
-                    self.last_save = time.time()
+                    with self.lock:
+                        time.sleep(1)
+                        self.click_on_element('//*[@id="prefsButton"]')
+                        self.click_on_element('//*[@id="menu"]/div[3]/div[3]/a[1]')
+                        new_save_code = self.get_text_from_element('//*[@id="textareaPrompt"]')
+                        open('save', 'w').write(new_save_code)
+                        self.click_on_element('//*[@id="promptOption0"]')
+                        self.click_on_element('//*[@id="menu"]/div[1]')
+                        self.last_save = time.time()
+            except Exception as e:
+                print(f'erro {e} no save game')
 
 
 
